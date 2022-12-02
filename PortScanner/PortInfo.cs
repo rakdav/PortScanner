160fe42bd45fc1d453e1dbe12aaa9eaed530eafe
rakdav <rakdav@inbox.ru>
2022-12-02 11:45:37 +0200

    Добавьте файлы проекта.

---
+++ PortScanner/PortInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortScanner
+{
+    internal class PortInfo
+    {
+        public int PortNumber { get; set; }
+        public string Local { get; set; }
+        public string Remote { get; set; }
+        public string State { get; set; }
+
+        public PortInfo(int portNumber, string local,
+            string remote, string state)
+        {
+            PortNumber = portNumber;
+            Local = local;
+            Remote = remote;
+            State = state;
+        }
+    }
+}
---
+++ PortScanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using PortScanner;
+using System.Net;
+using System.Net.NetworkInformation;
+List<PortInfo> list = GetOpenPort();
+foreach (var item in list)
+{
+    Console.WriteLine(item.PortNumber+" "+item.Local+" "
+        +item.Remote+" "+item.State);
+}
+
+List<PortInfo> GetOpenPort()
+{
+    IPGlobalProperties properties=IPGlobalProperties.GetIPGlobalProperties();
+    IPEndPoint[] tcpEndPoints=properties.GetActiveTcpListeners();
+    TcpConnectionInformation[] tcpConnectionInformation=
+        properties.GetActiveTcpConnections();
+    return tcpConnectionInformation.Select(p =>
+        {
+        return new PortInfo(p.LocalEndPoint.Port,
+            String.Format("{0}:{1}", p.LocalEndPoint.Address, p.LocalEndPoint.Port),
+            String.Format("{0}:{1}", p.RemoteEndPoint.Address, p.RemoteEndPoint.Port),
+            p.State.ToString()
+            );
+        }).ToList();
+}
